2385f7d3d7ef58ffac744e2d2f477c713430a27a
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-01-24 16:40:40 -0500

    Starting to break down C# CRUD

--- Program.cs
+++ Program.cs
@@ -2,11 +2,49 @@
 
 namespace NEWRhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        // Create:
+        // Greeting/Goodbye:
+        // Menu
+        // Add:
+        //            band
+        //            Albums for Bands
+        //            Song for Albums
+
+
+        // Read: View:
+        //            List of Bands
+        //            view band's albums
+        //            view all:
+        //               albums by release date
+        //               bands signed
+        //               bands not signed
+
+
+        // Update:
+        //       Resign a bands
+
+
+        // Delete:
+        //       Let band go
+
+
+        // * QUIT PROGRAM:
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
     }
-  }
 }
---
+++ README.md
@@ -0,0 +1,45 @@
+CRUD
+Create:
+Greeting/Goodbye
+Menu System
+Add:
+band
+Albums for Bands
+Song for Albums
+
+Read:
+View:
+List of Bands
+view band's albums
+view all:
+albums by release date
+bands signed
+bands not signed
+
+Update:
+Resign a bands
+
+Delete:
+Let band go
+
+QUIT PROGRAM:
+
+Create a menu system that shows the following options to the user until they choose to quit your program
+
+Add
+Add a new band
+Add an album for a band
+Add a song to an album
+
+Band Info
+View all the bands
+Prompt for a band name and view all their albums
+View all albums ordered by ReleaseDate
+View all bands that are signed
+View all bands that are not signed
+
+Delete/Remove Band
+Let a band go (update isSigned to false)
+Resign a band (update isSigned to true)
+
+Quit the program
